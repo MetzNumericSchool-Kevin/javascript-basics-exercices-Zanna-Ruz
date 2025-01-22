2ec320824aa8b59587a36c0fff45b21ae521cd03
Ruzanna <ruzannancy@gmail.com>
2025-01-22 10:01:46 +0100

    Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -1,4 +1,15 @@
 const nom_boutique = "SORCIER";
+
 let nombre_potions = 15;
 let prix_potion = 2;
 let boutique_ouvert = true;
+
+if (boutique_ouvert) {
+  console.log("​Bienvenue dans la boutique " + nom_boutique + " Aventurier ! ");
+} else {
+  console.log(
+    "​La boutique " +
+      nom_boutique +
+      " est fermée, revenez plus tard Aventurier "
+  );
+}
